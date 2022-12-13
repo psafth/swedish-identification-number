f0067b8cc90c9a4cec2a68feb62d87f1f238fd27
Fredrik Svensson <fredrik_s89@hotmail.com>
2022-12-13 23:39:38 +0100

    + Tests + Decade calculation when only partial year is given

---
+++ src/IdentificationNumber.Tests/PersonIdentificationNumberTest.cs
@@ -0,0 +1,18 @@
+using IdentificationNumber.Helpers;
+using IdentificationNumber.Models;
+
+namespace IdentificationNumber.Tests
+{
+    [TestClass]
+    public class PersonIdentificationNumber_Constructor
+    {
+        [TestMethod]
+        [DataRow("8907173572", true)]
+        [DataRow("890717+3572", true)]
+        public void Input_Valid_String(string input, bool expected)
+        {
+            var result = new PersonIdentificationNumber(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- src/IdentificationNumber/Entities/PersonIdentificationNumber.cs
+++ src/IdentificationNumber/Entities/PersonIdentificationNumber.cs
@@ -12,10 +12,7 @@ namespace IdentificationNumber.Models
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentNullException(nameof(value));
 
-            var personMatch = CommonRegex.MatchPerson(value);
-
-            if (!personMatch.Success)
-                throw new FormatException("The input does not match an valid person identification format.");
+            var number = Parse(value);
 
         }
 
@@ -42,9 +39,14 @@ namespace IdentificationNumber.Models
         /// <param name="match"></param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        private string EnsureFormat(Match match)
+        private string Parse(string value)
         {
-            var year = match.Groups["year"]?.Value;
+            var match = CommonRegex.MatchPerson(value);
+
+            if (!match.Success)
+                throw new FormatException("The input does not match an valid person identification format.");
+
+            var year = int.Parse(match.Groups["year"]?.Value);
             var month = int.Parse(match.Groups["month"]?.Value);
             var day = int.Parse(match.Groups["day"]?.Value);
             var separator = match.Groups["separator"]?.Value;
@@ -52,9 +54,32 @@ namespace IdentificationNumber.Models
             var control = int.Parse(match.Groups["control"]?.Value);
 
 
+            if (year < 1000)
+            {
+                var prevDecade = separator == "+" ? -100 : 0;
+
+                // Assume two digits
+                var assumedDateOfBirth = new DateTime(GetDecade(DateTime.Today.Year) + year, month, day > 31 ? day - 60 : day);
+                var dateOfBirth = assumedDateOfBirth < DateTime.Today ? assumedDateOfBirth : new DateTime(GetDecade(DateTime.Today.Year) + year - 100 + prevDecade, month, day > 31 ? day - 60 : day);
+            }
+            else
+            {
+                // Assume four digits
+            }
+
+            if (day > 31)
+            {
+                // Assume coordination number
+            }
 
 
             throw new NotImplementedException();
+        }
+
+
+        private int GetDecade(int year)
+        {
+            return Math.DivRem(year, 100, out int rem) * 100;
         }
 
         public string ConstructCenturyFigure(int year, int month, int day, bool overHundred = false)
