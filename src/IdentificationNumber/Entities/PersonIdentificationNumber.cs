93bd125f66b4bea9babfe51f91f34a1037c888d9
advfredriksvensson <fredrik.svensson@advania.com>
2022-12-14 17:22:22 +0100

    + Parsing + Tests

--- src/IdentificationNumber/Entities/PersonIdentificationNumber.cs
+++ src/IdentificationNumber/Entities/PersonIdentificationNumber.cs
@@ -1,19 +1,28 @@
 ﻿using IdentificationNumber.Helpers;
 using IdentificationNumber.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace IdentificationNumber.Models
 {
-    public class PersonIdentificationNumber : IdentificationNumber
+    public class PersonIdentificationNumber : IdentificationNumber, IEquatable<string>
     {
+        public DateTime DateOfBirth { get; private set; }
+
         public PersonIdentificationNumber(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentNullException(nameof(value));
 
-            var number = Parse(value);
+            _value = Parse(value, out DateTime dateOfBirth);
 
+            DateOfBirth = dateOfBirth;
+        }
+
+        public bool Equals(string other)
+        {
+            return _value == other;
         }
 
         public override bool Equals(IIdentificationNumber other)
@@ -21,7 +30,13 @@ namespace IdentificationNumber.Models
             throw new NotImplementedException();
         }
 
-        public override bool IsValid { get; }
+        public override bool IsValid
+        {
+            get
+            {
+                return Luhn.Validate(_value.Substring(2));
+            }
+        }
 
         public override string ToFriendlyName()
         {
@@ -30,7 +45,7 @@ namespace IdentificationNumber.Models
 
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return base._value;
         }
 
         /// <summary>
@@ -39,7 +54,7 @@ namespace IdentificationNumber.Models
         /// <param name="match"></param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        private string Parse(string value)
+        private string Parse(string value, out DateTime dateOfBirth)
         {
             var match = CommonRegex.MatchPerson(value);
 
@@ -59,45 +74,21 @@ namespace IdentificationNumber.Models
                 var prevDecade = separator == "+" ? -100 : 0;
 
                 // Assume two digits
-                var assumedDateOfBirth = new DateTime(GetDecade(DateTime.Today.Year) + year, month, day > 31 ? day - 60 : day);
-                var dateOfBirth = assumedDateOfBirth <= DateTime.Today ? assumedDateOfBirth : new DateTime(GetDecade(DateTime.Today.Year) + year - 100 + prevDecade, month, day > 31 ? day - 60 : day);
+                var assumedDateOfBirth = new DateTime(GetDecade(DateTime.Today.Year) + year + prevDecade, month, day > 31 ? day - 60 : day);
+                dateOfBirth = assumedDateOfBirth <= DateTime.Today ? assumedDateOfBirth : new DateTime(GetDecade(DateTime.Today.Year) + year - 100 + prevDecade, month, day > 31 ? day - 60 : day);
             }
             else
             {
                 // Assume four digits
+                dateOfBirth = new DateTime(year, month, day);
             }
 
-            if (day > 31)
-            {
-                // Assume coordination number
-            }
-
-
-            throw new NotImplementedException();
+            return $"{dateOfBirth.Year:0000}{dateOfBirth.Month:00}{day:00}{individual:000}{control:0}";
         }
-
 
         private int GetDecade(int year)
         {
             return Math.DivRem(year, 100, out int rem) * 100;
-        }
-
-        public string ConstructCenturyFigure(int year, int month, int day, bool overHundred = false)
-        {
-            if ((year < 0 && year > 99) || (year < 1000 && year > 9999))
-                throw new ArgumentOutOfRangeException(nameof(year), "Must be between 0 and 99 or 1000 and 9999");
-
-            if (month < 1 || month > 12)
-                throw new ArgumentOutOfRangeException(nameof(month), "Must be between 1 and 12");
-
-            if (day < 1 || (day > 32 && day < 61) || day > 91)
-                throw new ArgumentOutOfRangeException(nameof(day), "Must be between 1 and 31 or 61 and 92");
-
-            var currentCentury = DateTime.Today.Year;
-
-
-
-            throw new NotImplementedException();
         }
     }
 }
