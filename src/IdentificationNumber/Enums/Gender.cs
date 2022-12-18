61c62c4ba81f5ad7441c6a884cfd53d4d93e6d42
Fredrik Svensson <fredrik_s89@hotmail.com>
2022-12-18 20:58:19 +0100

    Feat: Gender for person identification number Test: Gender for person identification number

---
+++ src/IdentificationNumber/Enums/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentificationNumber.Enums
+{
+    public enum Gender
+    {
+        Female = 0,
+        Male = 1
+    }
+}
